80ce150daf2b0c830232c6f23da32d8a57073c12
Reiman, Jeremy (CCI-Southwest) <Jeremy.Reiman@cox.com>
2021-05-30 12:35:27 -0700

    Configured username and password to get obtained via command line arguments

--- san_switch.py
+++ san_switch.py
@@ -1,20 +1,24 @@
 from paramiko import SSHClient, AutoAddPolicy
 
-
-def ssh_run_single_command(hostname, username, password, command):
+def ssh_run_single_command(ip_address, username, password, command):
     ssh = SSHClient()
     ssh.set_missing_host_key_policy(AutoAddPolicy())
-    ssh.connect(hostname=hostname, port=22, username=username, password=password)
+    ssh.connect(hostname=ip_address, port=22, username=username, password=password)
     stdin, stdout, stderr = ssh.exec_command(command)
     output = stdout.readlines()
     output = [i.rstrip() for i in output]
     ssh.close()
     return output
 
-
 class Switch:
     def __init__(self, hostname, ip_address):
         self.hostname = hostname
         self.ip_address = ip_address
+    
+    def list_down_ports(self, username, password):
+        command_list_down_ports = "show interface brief | egrep -i 'fc' | egrep -v 'sup' | egrep -v 'up|trunking' | cut -d ' ' -f 1 | no-more"
+        output = ssh_run_single_command(self.ip_address, username, password, command_list_down_ports)
+        return output
+
 
 
--- san_switch_port_redundancy_check.py
+++ san_switch_port_redundancy_check.py
@@ -1,12 +1,16 @@
-from san_switch import ssh_run_single_command
 from san_switch import Switch
+from sys import argv
 
-
-command = "show interface brief | egrep -i 'fc' | egrep -v 'sup' | egrep -v 'up|trunking' | cut -d ' ' -f 1 | no-more"
+username = argv[1]
+password = argv[2]
 dukelabmds01 = Switch("dukelabmds01", "172.18.77.159")
+dukelabmds02 = Switch("dukelabmds02", "172.18.77.160")
 
 def main():
-    print(dukelabmds01.hostname)
+    dukelabmds01_down_ports_list = dukelabmds01.list_down_ports(username, password)
+    dukelabmds02_down_ports_list = dukelabmds02.list_down_ports(username, password)
+    print(dukelabmds01_down_ports_list)
+    print(dukelabmds02_down_ports_list)
 
 if __name__ == '__main__':
     main()
