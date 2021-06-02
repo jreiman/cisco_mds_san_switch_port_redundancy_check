691e11b69b8498717498970d73fd76e14f1dcab1
Reiman, Jeremy (CCI-Southwest) <Jeremy.Reiman@cox.com>
2021-06-02 00:46:11 -0700

    Added exceptions for empty lists to list_comparison.py

--- modules/list_comparison.py
+++ modules/list_comparison.py
@@ -1,5 +1,12 @@
 def find_differences_between_two_lists_and_sort_result(list1, list2):
-    a = set(list1)
-    b = set(list2)
-    c = a.symmetric_difference(b)
-    return sorted(list(c))
+    if not list1 and not list2:
+        raise ValueError("Both lists are empty.")
+    elif not list1:
+        raise ValueError("list1 is empty.")
+    elif not list2:
+        raise ValueError("list2 is empty.")
+    else:
+        a = set(list1)
+        b = set(list2)
+        c = a.symmetric_difference(b)
+        return sorted(list(c))
--- tests/test_list_comparison.py
+++ tests/test_list_comparison.py
@@ -9,7 +9,7 @@ class test_list_comparison(unittest.TestCase):
     list2_sorted = ['fc1/1', 'fc1/3', 'fc2/1', 'fc2/3']
     list2_unsorted = ['fc1/1', 'fc2/1', 'fc1/3', 'fc2/3']
     result_list1_list2 = ['fc1/2', 'fc1/3', 'fc2/2', 'fc2/3']
-    result_identical_list = []
+    empty_list = []
 
     def test_two_sorted_lists(self):
         self.assertEqual(differences(self.list1_sorted, self.list2_sorted), self.result_list1_list2)
@@ -18,11 +18,19 @@ class test_list_comparison(unittest.TestCase):
         self.assertEqual(differences(self.list1_unsorted, self.list2_unsorted), self.result_list1_list2)
 
     def test_two_identical_lists_in_same_order(self):
-        self.assertEqual(differences(self.list1_sorted, self.list1_sorted), self.result_identical_list)
+        self.assertEqual(differences(self.list1_sorted, self.list1_sorted), self.empty_list)
 
     def test_two_identical_lists_in_different_order(self):
-        self.assertEqual(differences(self.list2_sorted, self.list2_unsorted), self.result_identical_list)              
+        self.assertEqual(differences(self.list2_sorted, self.list2_unsorted), self.empty_list)              
 
+    def test_raise_exception_if_only_list1_is_empty(self):
+        self.assertRaises(ValueError, differences, self.empty_list, self.list2_sorted)
+
+    def test_raise_exception_if_only_list2_is_empty(self):
+        self.assertRaises(ValueError, differences, self.list1_sorted, self.empty_list)
+
+    def test_raise_exception_if_both_lists_are_empty(self):
+        self.assertRaises(ValueError, differences, self.empty_list, self.empty_list)
 
 if __name__ == '__main__':
     unittest.main()
